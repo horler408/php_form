789886c823701fb66934a1f3eb43cad6a41b4e18
horler408 <horler.web@gmail.com>
2021-03-19 21:29:17 +0100

    Adding index page

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+    <title>Home Page</title>
+</head>
+<body>
+    <header class="main-header">
+        <h1><span>HorlerTech</span> Portfolio</h1>
+        <p>Hi, my name is Abdulazeez Olaitan, A fullstack web developer</p>
+      </header>
+  
+      <main class="container">
+        <section class="card">
+          <img src="https://i.ibb.co/VvC0vpN/paint-1.png" alt="" />
+          <div>
+            <h3>Project One</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
+              enim laudantium, animi veniam libero eveniet culpa unde perferendis
+              illo fugit corporis, voluptatibus totam dolorum, maiores magnam
+              officia. Ab, delectus.
+            </p>
+            <a href="#" class="btn">Explore</a>
+          </div>
+        </section>
+  
+        <section class="card" data-aos="fade-left">
+          <img src="https://i.ibb.co/3NHjDcW/paint-2.png" alt="" />
+          <div>
+            <h3>Project Two</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
+              enim laudantium, animi veniam libero eveniet culpa unde perferendis
+              illo fugit corporis, voluptatibus totam dolorum, maiores magnam
+              officia. Ab, delectus.
+            </p>
+            <a href="#" class="btn">Explore</a>
+          </div>
+        </section>
+  
+        <section class="card" data-aos="fade-right">
+          <img src="https://i.ibb.co/0VywMkW/paint-3.png" alt="" />
+          <div>
+            <h3>Project <span>Three</span></h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
+              enim laudantium, animi veniam libero eveniet culpa unde perferendis
+              illo fugit corporis, voluptatibus totam dolorum, maiores magnam
+              officia. Ab, delectus.
+            </p>
+            <a href="#" class="btn">Explore</a>
+          </div>
+        </section>
+  
+        <section class="card" data-aos="fade-left">
+          <img src="https://i.ibb.co/5LkJFRP/paint-4.png" alt="" />
+          <div>
+            <h3>Project <span>Four</span></h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
+              enim laudantium, animi veniam libero eveniet culpa unde perferendis
+              illo fugit corporis, voluptatibus totam dolorum, maiores magnam
+              officia. Ab, delectus.
+            </p>
+            <a href="#" class="btn">Explore</a>
+          </div>
+        </section>
+  
+        <section class="card" data-aos="fade-right">
+          <img src="https://i.ibb.co/61R8Q2y/paint-5.png" alt="" />
+          <div>
+            <h3>Project <span>Five</span></h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod error
+              enim laudantium, animi veniam libero eveniet culpa unde perferendis
+              illo fugit corporis, voluptatibus totam dolorum, maiores magnam
+              officia. Ab, delectus.
+            </p>
+            <a href="#" class="btn">Explore</a>
+          </div>
+        </section>
+      </main>
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+        AOS.init({
+            offset: 400,
+            duration: 1000,
+            delay: 0
+        });
+    </script>
+</body>
+</html>
